37ca6c50de01ba33627b554a25607bb95feb42c1
Bodo Tasche <bodo@wannawork.de>
2012-10-13 14:31:29 +0200

    first batch of tests for lucas :)

--- spec/acceptance/spec_helper.rb
+++ spec/acceptance/spec_helper.rb
@@ -2,3 +2,13 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require "parsec"
+
+def check_parsing_result(parsed, street, number, city, zip, country, state)
+  parsed.street_name.should == street
+  parsed.city.should == city
+  parsed.street_number.should == number
+  parsed.zip.should == zip
+  parsed.country.should == country
+  parsed.state.should == state
+end
+
