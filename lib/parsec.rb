14a9ae80bc215433b0a3ea7e79a0f473be279b36
moonglum <moonglum@moonbeamlabs.com>
2012-10-20 11:22:37 +0200

    Some minor adjustments for the pull request #1
    * Give setup a symbol. Much more elegant than a String
    * Removed an unnecessary require statement
    * Removed the TODO - it belongs into Github Issues

--- lib/parsec.rb
+++ lib/parsec.rb
@@ -1,24 +1,21 @@
 require "parsec/version"
 require "parsec/knowledge"
 require "parsec/parsed_address"
-require "parsec/parser"
 
 module Parsec
-
   # Setup Parsec for parsing - just call it at startup
-  def Parsec.setup(country = "de")
-    @country = country
+  def Parsec.setup(country = :de)
+    @country = country.to_s
 
     # Load up country's knowledge file
-    knowledge_file = "../../raw_data/#{country.upcase}.txt"
-    Parsec::Knowledge.import File.expand_path(knowledge_file, __FILE__)
+    knowledge_file = "../../raw_data/#{@country.upcase}.txt"
+    Knowledge.import File.expand_path(knowledge_file, __FILE__)
 
-    # TODO: decouple common_knowledge from german Laender abbreviations
     Knowledge.common_knowledge
 
     # Load up the native and parser for this country
-    require "parsec/native/#{country}"
-    require "parsec/parser/#{country}_parser"
+    require "parsec/native/#{@country}"
+    require "parsec/parser/#{@country}_parser"
   end
 
   def Parsec.parse(raw_address)
